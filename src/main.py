aff86755615d5e3bd114bdea605fdb403eabf6cb
ashes999 <ashes999@yahoo.com>
2016-07-03 13:05:53 -0400

    Added instructions and monster names

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ class Main:
     """The main game loop logic. TODO: refactor into better classes"""
     
     def __init__(self):
-        pass
+        self.floor_number = 1
     
     def run(self):
-        print "Welcome to Legendary Tribble!"
+        self.print_instructions()
         
         num_monsters = int(random.uniform(3, 5))
         monsters = []
@@ -20,3 +20,16 @@ class Main:
         
         for m in monsters:
             print m
+            
+    def print_instructions(self):
+        print "Welcome to Legendary Tribble!"
+        print ""
+        
+        print "Instructions: type 'fight <name>' to fight a monster."
+        print "Kill all the monsters, then, type 'descend' to go to the next floor"
+        print "If you want to go back up one floor, type 'ascend'."
+        print ""
+        
+        print "You are on floor B{0}.".format(self.floor_number)
+        print ""
+    
--- src/monster.py
+++ src/monster.py
@@ -5,6 +5,12 @@ class Monster:
     
     def __init__(self):
         self.difficulty = int(random.uniform(1, 10))
+        if self.difficulty <= 3:
+            self.name = "rat"
+        elif self.difficulty <= 7:
+            self.name = "goblin"
+        else:
+            self.name = "ogre"
         
     def __str__(self):
-        return "Monster with difficulty {0}".format(self.difficulty)
+        return "A {0}".format(self.name)
