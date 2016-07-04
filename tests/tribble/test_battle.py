61db37f9e34b1a447afdf43dfe7734a71a44ba9c
ashes999 <ashes999@yahoo.com>
2016-07-04 18:47:40 -0400

    Switch from unittest to pytest

--- tests/tribble/test_battle.py
+++ tests/tribble/test_battle.py
@@ -2,7 +2,7 @@ import unittest
 
 from tribble.battle import Battle
 
-class BattleTests(unittest.TestCase):
+class TestBattle:
     def test_attack_decreases_targets_health(self):
         defender_health = 7
         attacker = Creature(10, 5, 3)
@@ -10,29 +10,17 @@ class BattleTests(unittest.TestCase):
         
         expected_damage = attacker.strength - defender.defense
         b = self.create_battle(attacker, defender)
-        b.attack(attacker, defender, 1)
+        b.attack(attacker, defender)
         
-        self.assertEqual(defender_health - expected_damage, defender.current_health)
-        
-    def test_attack_inflicts_damage_n_times(self):
-        n = 2
-        defender_health = 7
-        attacker = Creature(10, 5, 3)
-        defender = Creature(defender_health, 3, 1)
-        
-        expected_damage = attacker.strength - defender.defense
-        b = self.create_battle(attacker, defender)
-        b.attack(attacker, defender, n)
-        
-        self.assertEqual(defender_health - n * expected_damage, defender.current_health)
-        
+        assert defender.current_health == defender_health - expected_damage
+                
     def test_fight_it_out_terminates_when_player_dies(self):
         player = Creature(3, 3, 3)
         monster = Creature(10, 10, 10)
         b = self.create_battle(player, monster)
         b.fight_it_out()
         
-        self.assertLessEqual(player.current_health, 0)
+        assert player.current_health <= 0
         
     def test_fight_it_out_terminates_when_monster_dies(self):
         player = Creature(3, 3, 3)
@@ -40,7 +28,7 @@ class BattleTests(unittest.TestCase):
         b = self.create_battle(player, monster)
         b.fight_it_out()
         
-        self.assertLessEqual(monster.current_health, 0) 
+        assert monster.current_health <=  0 
         
     ### test helpers ###
         
