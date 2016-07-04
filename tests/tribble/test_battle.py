296a70ccb1af19523253755a1f51ed0744e9b91c
ashes999 <ashes999@yahoo.com>
2016-07-04 13:51:47 -0400

    Infrasturcture for unit tests

---
+++ tests/tribble/test_battle.py
@@ -0,0 +1,10 @@
+import unittest
+
+class BattleTests(unittest.TestCase):
+    def test_foo(self):
+        self.assertEqual(True, True)
+        
+    def test_bar(self):
+        self.assertEqual(1, 1)
+        
+    
